e0bc3c2b71d89a104ac5b9ff4273f43be3772cf3
Iloke Alusala <ilokealusala@Ilokes-MacBook-Pro.local>
2024-03-20 05:21:23 +0200

    Ran image legacy thingy

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'next/image'
+import Image from "next/legacy/image"
 import { HeartIcon } from '@heroicons/react/24/outline'
 import {StarIcon} from '@heroicons/react/24/outline'
 
